fd82aeeb0f09ae8b90270348e66b5a7bac94c869
ketsueki33 <harsh.karaiya@gmail.com>
2023-12-02 02:24:50 +0530

    finish up fixing of loading skeleton

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,17 +1,16 @@
-import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { Box, Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 function GameCardSkeleton() {
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Card maxWidth="600px" borderRadius={10} overflow="hidden">
-            <Skeleton height="300px" width="600px" />
-            <CardBody>
-              <SkeletonText />
-            </CardBody>
-          </Card>
-        </div>
-
-      );
+        <Box display="flex" justifyContent="center">
+            <Card maxWidth="600px" borderRadius={10} overflow="hidden">
+                <Skeleton height="300px" width="600px" />
+                <CardBody>
+                    <SkeletonText />
+                </CardBody>
+            </Card>
+        </Box>
+    );
 }
 
 export default GameCardSkeleton;
