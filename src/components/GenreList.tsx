98a1a2deda8af50ba6fc71146c91882667094764
ketsueki33 <harsh.karaiya@gmail.com>
2023-12-03 23:53:12 +0530

    clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,7 +4,9 @@ import {
     List,
     ListItem,
     Spinner,
-    Button,
+    Link,
+    Text,
+    Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import croppedImageUrl from "../services/image-url";
@@ -21,33 +23,33 @@ function GenreList({ onSelectGenre, selectedGenre }: Props) {
 
     if (error) return null;
     return (
-        <List>
-            {genres.map((genre) => (
-                <ListItem key={genre.id} paddingY="6px">
-                    <HStack>
-                        <Image
-                            boxSize="32px"
-                            borderRadius={8}
-                            src={croppedImageUrl(genre.image_background)}
-                        />
-                        <Button
-                            onClick={() => onSelectGenre(genre)}
-                            variant="link"
-                            fontSize="sm"
-                            fontWeight={
-                                selectedGenre?.id === genre.id
-                                    ? "bold"
-                                    : "lighter"
-                            }
-                        >
-                            {genre.name === "Massively Multiplayer"
-                                ? "MMO"
-                                : genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            <Heading fontSize="2xl" marginY={1}> Genres</Heading>
+            <List>
+                {genres.map((genre) => (
+                    <ListItem key={genre.id} paddingY="6px">
+                        <HStack as={Link} onClick={() => onSelectGenre(genre)}>
+                            <Image
+                                boxSize="36px"
+                                objectFit="cover"
+                                borderRadius={8}
+                                src={croppedImageUrl(genre.image_background)}
+                            />
+                            <Text
+                                fontSize="sm"
+                                fontWeight={
+                                    selectedGenre?.id === genre.id ? "bold" : "lighter"
+                                }
+                            >
+                                {genre.name === "Massively Multiplayer"
+                                    ? "MMO"
+                                    : genre.name}
+                            </Text>
+                        </HStack>
+                    </ListItem>
+                ))}
+            </List>
+        </>
     );
 }
 
